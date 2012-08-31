43daf6914c64aa7a035297f7072a015d9f396bc9
Adrian L Lange <adrianlund@gmail.com>
2012-09-01 01:31:44 +0200

    Majik!

---
+++ Collie.lua
@@ -0,0 +1,104 @@
+local filter = ''
+local filterTable = {}
+
+local function Update()
+	local scroll = MountJournal.ListScrollFrame
+	local offset = HybridScrollFrame_GetOffset(scroll)
+	local mounts = GetNumCompanions('MOUNT')
+
+	if(UnitLevel('player') < 20 or mounts < 1) then
+		scroll:Hide()
+		MountJournal.MountDisplay.NoMounts:Show()
+		MountJournal.selectedSpellID = 0
+		MountJournal_UpdateMountDisplay()
+		MountJournal.MountCount:SetText(0)
+		MountJournal.MountButton:SetEnabled(false)
+		return
+	else
+		scroll:Show()
+		MountJournal.MountDisplay.NoMounts:Hide()
+		MountJournal.MountButton:SetEnabled(true)
+	end
+
+	table.wipe(filterTable)
+
+	for index = 1, mounts do
+		local id, name, spell, icon, active = GetCompanionInfo('MOUNT', index)
+		if(name:lower():find(filter)) then
+			table.insert(filterTable, index)
+		end
+	end
+
+	local buttons = scroll.buttons
+	for j = 1, #buttons do
+		local button = buttons[j]
+		local index = j + offset
+		if(index <= #filterTable) then
+			local _, name, spell, icon, active = GetCompanionInfo('MOUNT', filterTable[index])
+			button.name:SetText(name)
+			button.icon:SetTexture(icon)
+			button.index = filterTable[index]
+			button.spellID = spell
+			button.active = active
+
+			if(active) then
+				button.DragButton.ActiveTexture:Show()
+			else
+				button.DragButton.ActiveTexture:Hide()
+			end
+
+			button:Show()
+
+			if(MountJournal.selectedSpellID == spell) then
+				button.selected = true
+				button.selectedTexture:Show()
+			else
+				button.selected = false
+				button.selectedTexture:Hide()
+			end
+
+			button:SetEnabled(true)
+
+			button.DragButton:SetEnabled(true)
+			button.additionalText = nil
+			button.icon:SetDesaturated(false)
+			button.icon:SetAlpha(1)
+			button.name:SetFontObject('GameFontNormal')
+
+			if(button.showingTooltip) then
+				MountJournalMountButton_UpdateTooltip(button)
+			end
+		else
+			button:Hide()
+		end
+	end
+
+	HybridScrollFrame_Update(scroll, #filterTable * 46, scroll:GetHeight())
+	MountJournal.MountCount.Count:SetText(mounts)
+end
+
+local function OnTextChanged(self)
+	local text = self:GetText()
+	if(text == SEARCH) then
+		filter = ''
+	else
+		filter = text
+	end
+
+	Update()
+end
+
+local Handler = CreateFrame('Frame')
+Handler:RegisterEvent('ADDON_LOADED')
+Handler:SetScript('OnEvent', function(self, event, name)
+	if(name == 'Blizzard_PetJournal') then
+		local Search = CreateFrame('EditBox', 'MountSearch', MountJournal, 'SearchBoxTemplate')
+		Search:SetSize(145, 20)
+		Search:SetPoint('LEFT', MountJournal.MountCount, 'RIGHT', 15, 0)
+		Search:SetMaxLetters(40)
+		Search:SetScript('OnTextChanged', OnTextChanged)
+
+		MountJournal.ListScrollFrame.update = Update
+		MountJournal_UpdateMountList = Update
+	end
+end)
